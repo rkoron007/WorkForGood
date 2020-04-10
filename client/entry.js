04d6c896c16c86132ce7298803e9c171aa7e4139
Rose <rkoron007@gmail.com>
2020-04-09 17:27:36 -0700

    more specfic import

--- client/entry.js
+++ client/entry.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app";
+import App from "./components/app.js";
 import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
