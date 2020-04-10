f20c66616cefa2e698d240928860e9996abc26a3
Rose <rkoron007@gmail.com>
2020-04-09 17:29:56 -0700

    Try change file name

--- client/entry.js
+++ client/entry.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app.js";
+import App from "./components/App";
 import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
